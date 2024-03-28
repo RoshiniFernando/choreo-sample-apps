8d8d4536a53e5d91e200f55e6f3566abbb4f7d59
RoshiniFernando <iresharoshini7@gmail.com>
2024-03-28 09:05:25 +0000

    Update port

--- python/flask-app/app.py
+++ python/flask-app/app.py
@@ -87,4 +87,4 @@ def updateReservation(reservationId, request):
     return reservationId
 
 if __name__ == "__main__":
-    app.run(port=9090, host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
